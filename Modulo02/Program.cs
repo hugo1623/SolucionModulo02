a74ef775c727940b9555f8acf15a6988f80dcbef
Hugo Luis <hugoluis.system@gmail.com>
2021-04-04 23:42:58 -0500

    Add project files.

---
+++ Modulo02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modulo02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v2 = new vector(new List<int> { 1, 2 });
+        }
+        class vector
+        {
+            private List<int> _valores;
+            //Costructor
+            public vector(List<int> valores)
+            {
+                _valores= valores ;
+            }
+        }
+
+            
+    }
+}
