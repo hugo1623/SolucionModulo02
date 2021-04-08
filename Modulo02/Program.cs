116ee23859bab3d684abfabac4f06c3e47cb1116
Hugo Luis <hugoluis.system@gmail.com>
2021-04-07 23:55:07 -0500

    Métodos | Clases y objetos

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -6,53 +6,50 @@ namespace Modulo02
     class Program
     {
         static void Main(string[] args)
-        {
+        {   var v1 = new vector(new List<int> { 3, 4 });
             var v2 = new vector(new List<int> { 1, 2 });
-            Console.WriteLine(v2.Componentes.ToString());
-            Console.WriteLine(v2.Dimension);
-            v2[0] = 5;
-            Console.WriteLine("Indexador");
-            Console.WriteLine(v2[0]);
-            Console.WriteLine(v2.Componentes[0]);
-            v2.Nombre = "Vector 1";
-            Console.WriteLine(v2.Nombre);
+            vector vectorSuma = v1.Suma(v2);
             Console.Read();
         }
+    }
         class vector
-        {
-            //campo
+        {   //campo
             private List<int> _componentes;
+            public List<int> Componentes
+        {
+            get
+            {
+                return _componentes;
+            }
+        }
+            //Indexador
             public int this[int i]
             {
-                get
-                {
-                    return _componentes[i];                
-                }
-                set
-                {
-                    _componentes[i] = value;
-                }
+                get{ return _componentes[i];}
+                set{ _componentes[i] = value;}
             }
 
             public int Dimension { get { return _componentes.Count; } }
-
-            public string Nombre { get; set; }
-
-            public List<int> Componentes
-            {
-                get
-                {
-                    return _componentes;
-                }
-            }
-
-            //Costructor
+             //Costructor
             public vector(List<int> componentes)
             {
                 _componentes = componentes;
             }
-        }
 
-            
-    }
+            public vector Suma(vector v2)
+            {
+                if(Dimension != v2.Dimension)
+                {
+                    throw new ApplicationException("La dimensiones no son iguales");
+                }
+                List<int> resultado = new List<int>();
+
+                for(int i=0; i< Dimension; i++)
+                {
+                    resultado.Add(this[i] + v2[i]);
+                }
+
+                return new vector(resultado);
+            }
+        }   
 }
