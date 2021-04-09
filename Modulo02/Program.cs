766f0c7266674ea16e38c2dd1ffb7ad2e95eb5ba
Hugo Luis <hugoluis.system@gmail.com>
2021-04-08 23:31:51 -0500

    Operadores en las Clases

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -6,37 +6,40 @@ namespace Modulo02
     class Program
     {
         static void Main(string[] args)
-        {   var v1 = new vector(new List<int> { 3, 4 });
-            var v2 = new vector(new List<int> { 1, 2 });
-            vector vectorSuma = v1.Suma(v2);
+        {   var v1 = new Vector(new List<int> { 3, 4 });
+            var v2 = new Vector(new List<int> { 1, 2 });
+            Vector vectorSuma = v1.Suma(v2);
+            Vector vectorSuma2 = v1 + v2;
             Console.Read();
         }
     }
-        class vector
-        {   //campo
-            private List<int> _componentes;
-            public List<int> Componentes
+    class Vector
+    {   //campo
+        private List<int> _componentes;
+        public List<int> Componentes
         {
             get
             {
                 return _componentes;
             }
         }
-            //Indexador
-            public int this[int i]
-            {
-                get{ return _componentes[i];}
-                set{ _componentes[i] = value;}
-            }
-
-            public int Dimension { get { return _componentes.Count; } }
-             //Costructor
-            public vector(List<int> componentes)
-            {
-                _componentes = componentes;
-            }
-
-            public vector Suma(vector v2)
+        //Indexador
+        public int this[int i]
+        {
+            get { return _componentes[i]; }
+            set { _componentes[i] = value; }
+        }
+        public int Dimension { get { return _componentes.Count; } }
+        //Costructor
+        public Vector(List<int> componentes)
+        {
+            _componentes = componentes;
+        }
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+         return vector1.Suma(vector2);
+        }
+            public Vector Suma(Vector v2)
             {
                 if(Dimension != v2.Dimension)
                 {
@@ -48,8 +51,7 @@ namespace Modulo02
                 {
                     resultado.Add(this[i] + v2[i]);
                 }
-
-                return new vector(resultado);
+                return new Vector(resultado);
             }
         }   
 }
