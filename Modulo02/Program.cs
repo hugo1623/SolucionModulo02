d493572ced7c8388b257148e45e31f0f23fbb87e
Hugo Luis <hugoluis.system@gmail.com>
2021-04-12 23:41:32 -0500

    Métodos de Extensión | Clases y Objetos

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -7,36 +7,26 @@ namespace Modulo02
     {
         static void Main(string[] args)
         {
-            var e1 = new Empresa();
-            e1._NombreLegal = "Primera Empresa";
-            e1._Direccion = "Primera Direccion";
-
-            var e2 = new Empresa();
-            e2._NombreLegal = "Segunda Empresa";
-            e2._Direccion = "Segunda Direccion";
-
-            e1.metodo1(e1);
-            e2.metodo1(e1);
-            e1.metodo1(e1);
+            Console.WriteLine( 5.ElevadoALa(3));
+            Console.WriteLine(7.ElevadoALa(2));
+            Console.WriteLine(15.Double());
+            Console.Read();
         }
-        class Empresa
-        {
-            public string _NombreLegal;
-            public string _Direccion;
-            public void metodo1(Empresa empresa2)
-            {
-                var this_nombreLegal = this._NombreLegal;
-                var this_direccion = this._Direccion;
-                var miDireccion = _Direccion;
-                var miNombreLegal = _NombreLegal;
-                var empresa2_nombreLegal = empresa2._NombreLegal;
-                var empresa2_direccion = empresa2._Direccion;
-                     
-            }
-
-        }
-        
-            
     }
+
+        public static class   IntegerExtensionMethods
+        {
+            public static double ElevadoALa(this int valor, int exponente)
+                {
+                    return Math.Pow(valor, exponente);
+                }
+            public static double Double(this int valor)
+                {
+                    return valor * 2;
+                }
+
+        }
+       
+    
 
 }
