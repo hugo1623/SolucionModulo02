68a49baba184aa51bcde2625808384b50db1fd45
Hugo Luis <hugoluis.system@gmail.com>
2021-04-27 16:00:32 -0500

    Clase Abstracta

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -7,26 +7,51 @@ namespace Modulo02
     {
         static void Main(string[] args)
         {
-            var animalito1 = new { animal = "perro", nombre = "Max", vidas = 1 };
-            var animalito2 = new { animal = "gato", nombre = "Kyra", vidas = 7 };
-            //dynamic es un truco de C# para permitirns utilixar tipos variables.
-            List<dynamic> animalitos = new List<dynamic>();
+            Animal perro = new Perro();
+            Animal gato = new Gato();
 
-            animalitos.Add(animalito1);
-            animalitos.Add(animalito2);
-
-            foreach (dynamic animalito in animalitos)
-            {
-                Console.WriteLine("El {0} de nombre {1} tiene {2} vidad",
-                    animalito.animal,animalito.nombre, animalito.vidas);
-            }
-            Console.Read();
+            AnimalHacerRuido(perro);
+            AnimalHacerRuido(gato);
+        }
+        private static void AnimalHacerRuido(Animal animal)
+        {
+            animal.HacerRuido();
         }
     }
-    //Clase persona
-    class Persona
+    
+   abstract class Animal
     {
-        public string Nombre { get; set; }
-        public int Edad { get; set; }
+        public virtual void HacerRuido()
+        {
+            Console.WriteLine("Ruido generico");
+        }
+        protected void MetodoProtegido()
+        {
+
+        }
     }    
+    class Perro: Animal
+    {
+        public override void HacerRuido()
+        {
+            Console.WriteLine("Woof");
+            MetodoProtegido();
+        }
+        public void ElBaile()
+        {
+            Console.WriteLine(" Del Perrito");
+        }
+    }
+    class Gato: Animal
+    {
+        public override void HacerRuido()
+        {
+            Console.WriteLine("Miauu");
+            MetodoProtegido();
+        }
+        public void ElBaile()
+        {
+            Console.WriteLine(" Del Gato");
+        }
+    }
 }
