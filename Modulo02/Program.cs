e4c9471e39e7e43279b52bba2b8f63d1912bef79
Hugo Luis <hugoluis.system@gmail.com>
2021-04-29 02:09:56 -0500

    Arreglos

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -8,31 +8,25 @@ namespace Modulo02
     {
         static void Main(string[] args)
         {
-            HashSet<int> conjunto1 = new HashSet<int>();
+            //instanciar una lista y le pase elementos , puedo pasr cuantos elementos quiera
+            List<int> numeros = new List<int>();
+            numeros.Add(1);
+            numeros.Add(2);
 
-            conjunto1.Add(5);
-            conjunto1.Add(7);
+            //Arreglos de numeros y el numero de elementos que tendra el arreglo =3.
+            int[] arregloDeNumeros = new int[3];
+            arregloDeNumeros[0] = 15;
+            arregloDeNumeros[1] = 12;
+            arregloDeNumeros[2] = 26;
 
-            HashSet<int> conjunto2 = new HashSet<int>();
+            //Arreglo de string, se infiere el numero de elementos dentro de los parentesis
+            string[] arregloDeString = new string[] { "luis", "C#", "Angular", "...", ".NET" };
+            arregloDeString[4] = "Pyton";
 
-            conjunto2.Add(5);
-            conjunto2.Add(8);
-            //union=5,7,8
-            HashSet<int> union_set1_set2 = new HashSet<int>(conjunto1);
-            union_set1_set2.UnionWith(conjunto2);
-            //interseccion=5
-            HashSet<int> interseccion_set1_set2 = new HashSet<int>(conjunto1);
-            interseccion_set1_set2.IntersectWith(conjunto2);
-            //diferencia de aquellos elementos que no se encuentran en el conjunto1=7
-            HashSet<int> diferencia_set1_set2 = new HashSet<int>(conjunto1);
-            diferencia_set1_set2.ExceptWith(conjunto2);
-            // Diferencia entre los 2 conjuntos= 7,8
-            HashSet<int> diferencia_simetrica_set1_set2 = new HashSet<int>(conjunto1);
-            diferencia_simetrica_set1_set2.SymmetricExceptWith(conjunto2);
+            //Redimencionando un arreglo de 5 elementos a 6
+            Array.Resize<string>(ref arregloDeString, 6);
 
-            //Utilizando un conjunto de elementos vamos a eliminar los elementos comunes de las lista
-            List<int> numeros = new List<int>() { 1, 2, 3, 5, 6, 7, 3, 2, 2, 1, 3, 42, 2, 3, 34, 2, 2, 2, 3 };
-            numeros = new HashSet<int>(numeros).ToList();
+            arregloDeString[6] = "Magento";
         }
     }
  
