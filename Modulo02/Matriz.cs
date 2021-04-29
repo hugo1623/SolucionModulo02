4e3bd4c637b0c929bd39ce023ba542a71aee1c6f
Hugo Luis <hugoluis.system@gmail.com>
2021-04-29 05:35:52 -0500

    Colecciones y Arreglos

---
+++ Modulo02/Matriz.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modulo02
+{
+    public class Matriz
+    {
+        private int[,] _data;
+        public Matriz(int Filas, int Columnas)//ctor
+        {
+            _data = new int[Filas, Columnas];
+        }
+
+        public int Filas { get { return _data.GetLength(0); } }
+        public int Columnas { get { return _data.GetLength(1); } }
+        public int this[int fila, int columna]
+        {
+            get
+            {
+                return _data[fila, columna];
+            }
+            set
+            {
+                _data[fila, columna] = value;
+            }
+        }
+        public static Matriz operator +(Matriz matrizA, Matriz matrizB)
+        {
+            if(matrizA.Filas != matrizB.Filas || matrizA.Columnas != matrizB.Columnas)
+            {
+                throw new ApplicationException("Las matrices deben ser de la misma dimención");
+            }
+            Matriz resultado = new Matriz(matrizA.Filas, matrizA.Columnas);
+            for(int i=0; i< matrizA.Filas;i++)
+            {
+                for(int j=0;j< matrizA.Columnas;j++)
+                {
+                    resultado[i, j] = matrizA[i, j] + matrizB[i, j];
+                }
+            }
+            return resultado;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var tmpFila = new int[Filas];
+
+            for (int i = 0; i < Filas; i++)
+            {
+                for (int j = 0; j < Columnas; j++)
+                {
+                    tmpFila[j] = this[i, j];
+                }
+                sb.AppendLine(string.Join("\t", tmpFila));
+                sb.AppendLine("");
+            }
+            return sb.ToString();            
+        }
+
+    }
+}
--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -7,56 +7,49 @@ namespace Modulo02
     {
         static void Main(string[] args)
         {
-            int[,] matriz = new int[2, 2];
+            Matriz matriz = new Matriz(2, 2);
             matriz[0, 0] = 5;
-            matriz[0, 1] = 2;
-            matriz[1, 0] = 3;
-            matriz[1, 1] = 5;
+            matriz[0, 1] = -5;
+            matriz[1, 0] = 1;
+            matriz[1, 1] = 4;
 
-            int[,] matrizB = new int[2, 2]
-            {
-                {3,2 },{1,3}
-            };
+            Matriz matrizB = new Matriz(2, 2);
+            matrizB[0, 0] = 1;
+            matrizB[0, 1] = 6;
+            matrizB[1, 0] = 3;
+            matrizB[1, 1] = 10;
 
-            string[,] matrizC = new string[3, 2]
-            {
-                { "valor1","valor2"},
-                { "valor3", "valor4"},
-                { "valor5","valor6"},
-            };
-            EscribirArregloBidimencionalEnPantalla(matriz);
+            Console.WriteLine("Matriz 1");
+            Console.WriteLine(matriz.ToString());
+
+            Console.WriteLine("");
+            Console.WriteLine("Matriz 2");
+            Console.WriteLine(matrizB.ToString());
+
+            Console.WriteLine("");
+            Console.WriteLine("Matriz Resultante");
+            Matriz matrizSuma = matriz + matrizB;
+            Console.WriteLine(matrizSuma.ToString());
+
             Console.Read();
         }
          //Metodo para visualizar la matriz
-        public static void EscribirArregloBidimencionalEnPantalla(int[,] matriz)
+        public static void EscribirArregloBidimencionalEnPantalla<T>(T[,] matriz)
         {
-        //Obteniendo las dimensiones de las filas y las columna utitizando el metodo GerLength,
-        //0->Dimencion de las Filas y 1-> dimencion de las columnas, cuantas filas y columnas tiene
             var filas = matriz.GetLength(0);
-            var columnas = matriz.GetLength(1);// 
-
-            //inicializamos un stringBuilder,nos ayuda a construir paso a paso un string, nos sirve
-            //para ir costruyendo fila a fila el resultado de la lectura
+            var columnas = matriz.GetLength(1);
             var sb = new StringBuilder();
+            var tmpFila = new T[matriz.GetLength(0)];
 
-            //hacemos una variable temporal
-            var tmpFila = new int[matriz.GetLength(1)];
-
-            //realizamos un for para iterar por fila
             for(int i=0;i<filas;i++)
             {
-                //realizamos un for para iterar las columnas
                 for (int j=0;j<columnas;j++)
                 {
-                    //llenamos la fila temporal con los valores de la fila actual
                     tmpFila[j] = matriz[i, j];
                 }
-                // a la fila actual le hacemos un Join para que nos escriba los valores en secuenci
                 sb.AppendLine(string.Join("\t", tmpFila));
-                //agregamos una fila vacia
                 sb.AppendLine("");
             }
-            //escribo en consola
             Console.WriteLine(sb.ToString());
         }
     }
