b7a4ad26a80b382858697843a32e48b20e423005
Hugo Luis <hugoluis.system@gmail.com>
2021-04-28 21:49:03 -0500

    FIFO con Queue

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -4,27 +4,28 @@ namespace Modulo02
 {
     class Program
     {
-        static void Main(String[] arg)
+        static void Main(String[] args)
         {
-            List<string> nombres = new List<string>();
-            nombres.Add("Hugo");
-            nombres.Add("Luis");
-            nombres.Add("5");
+            Queue<string> queue = new Queue<string>();
 
-            List<int> numeros = new List<int>() { 1, 2, 3, 4, 5, 6 };
-            numeros.Add(0);
-            // remueve un elemento en especifico Nro 3 ahora la lista sera {1,2,4,5,6}
-            numeros.Remove(3);
-            // remueve el primer elemeneto de indice 0 ={2,3,4,5,6}
-            numeros.RemoveAt(0);
-            // remueve el ultimo elemento 
-            numeros.RemoveAt(numeros.Count - 1);
-            //insertar un elemento en una posicion especifica
-            numeros.Insert(0, 100);
-            numeros.Insert(4, -100);
+            Console.WriteLine("Primer Elemento Insertado:");
+            Console.WriteLine("Primero");
+            queue.Enqueue("Primero");
 
-            var primerelementoDeLaLista = numeros[0];
+            Console.WriteLine("Segundo Elemento Insertado:");
+            Console.WriteLine("Segundo");
+            queue.Enqueue("Segundo");
 
+            Console.WriteLine("Tercero Elemento Insertado:");
+            Console.WriteLine("Tercero");
+            queue.Enqueue("Tercero");
+            /*Podemos iterar el queue para leer su contenido*/
+            /*el queue esta vacio en este momento*/
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
             Console.Read();
         }
     }
