4f0a83a27885318877aaf820ad3e8bb50935992e
Hugo Luis <hugoluis.system@gmail.com>
2021-04-29 04:11:48 -0500

    Arreglos Bidimensionales

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 
 namespace Modulo02
 {
@@ -8,25 +7,57 @@ namespace Modulo02
     {
         static void Main(string[] args)
         {
-            //instanciar una lista y le pase elementos , puedo pasr cuantos elementos quiera
-            List<int> numeros = new List<int>();
-            numeros.Add(1);
-            numeros.Add(2);
+            int[,] matriz = new int[2, 2];
+            matriz[0, 0] = 5;
+            matriz[0, 1] = 2;
+            matriz[1, 0] = 3;
+            matriz[1, 1] = 5;
 
-            //Arreglos de numeros y el numero de elementos que tendra el arreglo =3.
-            int[] arregloDeNumeros = new int[3];
-            arregloDeNumeros[0] = 15;
-            arregloDeNumeros[1] = 12;
-            arregloDeNumeros[2] = 26;
+            int[,] matrizB = new int[2, 2]
+            {
+                {3,2 },{1,3}
+            };
 
-            //Arreglo de string, se infiere el numero de elementos dentro de los parentesis
-            string[] arregloDeString = new string[] { "luis", "C#", "Angular", "...", ".NET" };
-            arregloDeString[4] = "Pyton";
+            string[,] matrizC = new string[3, 2]
+            {
+                { "valor1","valor2"},
+                { "valor3", "valor4"},
+                { "valor5","valor6"},
+            };
+            EscribirArregloBidimencionalEnPantalla(matriz);
+            Console.Read();
+        }
+         //Metodo para visualizar la matriz
+        public static void EscribirArregloBidimencionalEnPantalla(int[,] matriz)
+        {
+        //Obteniendo las dimensiones de las filas y las columna utitizando el metodo GerLength,
+        //0->Dimencion de las Filas y 1-> dimencion de las columnas, cuantas filas y columnas tiene
+            var filas = matriz.GetLength(0);
+            var columnas = matriz.GetLength(1);// 
 
-            //Redimencionando un arreglo de 5 elementos a 6
-            Array.Resize<string>(ref arregloDeString, 6);
+            //inicializamos un stringBuilder,nos ayuda a construir paso a paso un string, nos sirve
+            //para ir costruyendo fila a fila el resultado de la lectura
+            var sb = new StringBuilder();
 
-            arregloDeString[6] = "Magento";
+            //hacemos una variable temporal
+            var tmpFila = new int[matriz.GetLength(1)];
+
+            //realizamos un for para iterar por fila
+            for(int i=0;i<filas;i++)
+            {
+                //realizamos un for para iterar las columnas
+                for (int j=0;j<columnas;j++)
+                {
+                    //llenamos la fila temporal con los valores de la fila actual
+                    tmpFila[j] = matriz[i, j];
+                }
+                // a la fila actual le hacemos un Join para que nos escriba los valores en secuenci
+                sb.AppendLine(string.Join("\t", tmpFila));
+                //agregamos una fila vacia
+                sb.AppendLine("");
+            }
+            //escribo en consola
+            Console.WriteLine(sb.ToString());
         }
     }
  
