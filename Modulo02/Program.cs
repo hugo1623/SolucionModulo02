7cc85d517c829b7b30cd811c6adbc0b3927dabb4
Hugo Luis <hugoluis.system@gmail.com>
2021-04-30 23:22:23 -0500

    Jagged Array o Arreglo de Arreglos

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -7,51 +7,23 @@ namespace Modulo02
     {
         static void Main(string[] args)
         {
-            Matriz matriz = new Matriz(2, 2);
-            matriz[0, 0] = 5;
-            matriz[0, 1] = -5;
-            matriz[1, 0] = 1;
-            matriz[1, 1] = 4;
+            int[][] jaggeArray = new int[2][];
+            jaggeArray[0] = new int[4];
+            jaggeArray[0][0] = 6;
+            jaggeArray[0][1] = -3;
+            jaggeArray[0][2] = 9;
+            jaggeArray[0][3] = -150;
+            jaggeArray[1] = new int[]
+            {
+                15,6,3,2,6,7,8,-100
+            };
 
-            Matriz matrizB = new Matriz(2, 2);
-            matrizB[0, 0] = 1;
-            matrizB[0, 1] = 6;
-            matrizB[1, 0] = 3;
-            matrizB[1, 1] = 10;
-
-            Console.WriteLine("Matriz 1");
-            Console.WriteLine(matriz.ToString());
-
-            Console.WriteLine("");
-            Console.WriteLine("Matriz 2");
-            Console.WriteLine(matrizB.ToString());
-
-            Console.WriteLine("");
-            Console.WriteLine("Matriz Resultante");
-            Matriz matrizSuma = matriz + matrizB;
-            Console.WriteLine(matrizSuma.ToString());
-
+            for (int i = 0; i < jaggeArray.GetLength(0); i++)
+            {
+                var join = string.Join(",", jaggeArray[i]);
+                Console.WriteLine(join);
+            }
             Console.Read();
         }
-         //Metodo para visualizar la matriz
-        public static void EscribirArregloBidimencionalEnPantalla<T>(T[,] matriz)
-        {
-            var filas = matriz.GetLength(0);
-            var columnas = matriz.GetLength(1);
-            var sb = new StringBuilder();
-            var tmpFila = new T[matriz.GetLength(0)];
-
-            for(int i=0;i<filas;i++)
-            {
-                for (int j=0;j<columnas;j++)
-                {
-                    tmpFila[j] = matriz[i, j];
-                }
-                sb.AppendLine(string.Join("\t", tmpFila));
-                sb.AppendLine("");
-            }
-            Console.WriteLine(sb.ToString());
-        }
     }
- 
 }
