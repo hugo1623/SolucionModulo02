e84457eaf8d404e048a44f5601fc779905a59959
Hugo Luis <hugoluis.system@gmail.com>
2021-05-02 02:50:22 -0500

    Predicados

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -5,39 +5,25 @@ namespace Modulo02
 {
     class Program
     {
-        public delegate string DeEnteroAString(int valor);
         static void Main(string[] args)
         {
-
-            var deEnteroAString = new DeEnteroAString(FuncionRetornaToString);
-            var deEnteroAStringMasUno = new DeEnteroAString(FuncionRetornaToStringMas1);
-
-            Console.WriteLine(deEnteroAString(10));
-            Console.WriteLine(deEnteroAStringMasUno(10));
-            Func<int, string> delegateFunc = FuncionRetornaToStringMas1;
-            var resultado = delegateFunc(15);
-            Action<int, string> delegadoAction = Ejemplo;
-            Template(deEnteroAString, 10);
+            Predicate<Persona> predicado = EsMayorDeEdad;
+            var p = new Persona()
+            {
+                Nombre = "Luis",
+                Edad = 26
+            };
+            Console.WriteLine(predicado(p));
             Console.Read();
         }
-        public static string FuncionRetornaToString(int valor)
+        static bool EsMayorDeEdad(Persona persona)
         {
-            return valor.ToString();
+            return persona.Edad >= 18;
         }
-        public static string FuncionRetornaToStringMas1(int valor)
-        {
-            return (valor+1).ToString();
-        }
-        public static void Ejemplo(int a, string b)
-        {
-
-        }
-        public static void Template(DeEnteroAString algororitmo, int valor)
-        {
-            Console.WriteLine("Hacer algo antes del algoritmo");
-            Console.WriteLine(algororitmo(valor));
-            //algoritmo
-            Console.WriteLine("Hacer algo despues del algoritmo");
-        }
+    }
+    public  class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
     }
 }
