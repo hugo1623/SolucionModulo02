2e4cd2226ffbfbfeb9f34b7e3f6dd4b92f404095
Hugo Luis <hugoluis.system@gmail.com>
2021-05-04 23:43:25 -0500

    LINQ: Reverse, reversar lista

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -13,18 +13,10 @@ namespace Modulo02
             {
                 3,5,7,8,2,-3,-100,523,6,22
             };
-            // OrderByDescending : para hacerlo descendente
-            var numerosMayoresQueCero = numeros.Where(x => x > 0).OrderByDescending(x => x).ToList();
+            //Permite revertir los elementos del medio el 1=3 y el 8=22 no los toca
+            numeros.Reverse(1,8);
 
-            List<Persona> personas = new List<Persona>()
-            {
-                new Persona() { Nombre = "Hugo", Edad = 26, Salario=5 },
-                new Persona(){Nombre ="Kyra", Edad=10, Salario=7},
-                new Persona(){Nombre ="lulu", Edad=13, Salario=10 },
-                new Persona(){Nombre ="Dayron", Edad=15,Salario=1}
-            };
-            //ThenBy:toma el resultado de un OrderBy y sigue ordenando en base a ese resultado 
-            var listaDePersonasOrdenadas = personas.OrderBy(x => x.Salario).ThenBy(x => x.Nombre).ToString();
+           
             Console.Read();
         }
     }
