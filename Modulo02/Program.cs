2f4a2f336620e5ff54c00369c73b9232f368addf
Hugo Luis <hugoluis.system@gmail.com>
2021-05-06 03:50:08 -0500

    LINQ: group by

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -13,10 +13,19 @@ namespace Modulo02
             {
                 3,5,7,8,2,-3,-100,523,6,22
             };
-            var listado = numeros.TakeWhile(x=> x<6).ToList() ;
-            var listado2 = numeros.SkipWhile(x => x < 6).ToList();
+            var grupos2= numeros.GroupBy(x=>Math.Abs(x%2)) ;
+            
+            foreach(var clase in grupos2)
+            {
+                Console.WriteLine("Numeros cuyo residuo es " + clase.Key);
+                foreach(var numero in clase)
+                {
+                    Console.WriteLine(numero);
+                }
+                Console.WriteLine("");
+            }
 
-
+            // Ejemplo2: Separando por grupos de Edad
             List<Persona> personas = new List<Persona>()
             {
                 new Persona() { Nombre = "Hugo", Edad = 26, Salario=5 },
@@ -24,14 +33,24 @@ namespace Modulo02
                 new Persona(){Nombre ="lulu", Edad=13, Salario=10},
                 new Persona(){Nombre ="Dayron", Edad=15,Salario=5}
             };
-            var nombre = personas.Where(x => x.Edad < 18).Select(x =>
-            new
+            var GrupoDePersonas = personas.GroupBy(X =>
             {
-                Edad=x.Edad,
-                Nombre=x.Nombre,
-                Salario=x.Salario
+                if(X.Edad <=20)
+                { return "Menor que 20";}
+                else if (X.Edad >= 21 && X.Edad <=40)
+                {return "Entre 21 y 40";}
+                else
+                {return "Mayores que 41";}
+            });
+            foreach (var grupoPersona in GrupoDePersonas)
+            {
+                Console.WriteLine("Grupo de:" + grupoPersona.Key + "---cantidad : "+ grupoPersona.Count());
+                foreach (var persona in grupoPersona)
+                {
+                    Console.WriteLine(persona.Nombre);
+                }
+                Console.WriteLine("");
             }
-            ).FirstOrDefault();
             Console.Read();
         }
     }
