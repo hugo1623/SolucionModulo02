42b86de45c70a7122024d3fb4b8df13e78b47169
Hugo Luis <hugoluis.system@gmail.com>
2021-05-10 02:30:19 -0500

    Connection Strings y conectar a SQL Server

--- Modulo02/Program.cs
+++ Modulo02/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.Data.SqlClient;
+//using System.Data.SqlClient;
+
 namespace Modulo02
 {
     class Program
     {
-        public static object ConfigurationManager { get; private set; }
 
         static void Main(string[] args)
         {
-            /*var mensaje= ConfigurationManager.AppSettings["mensaje"]
-            Console.WriteLine(mensaje);
-            Console.ReadLine();*/
+            /*var mensaje = ConfigurationManager.AppSettings["mensaje"];
+              Console.WriteLine(mensaje);*/
+
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"DESKTOP-H4CI0FL";
+            builder.InitialCatalog = "Prueba";
+            builder.IntegratedSecurity = false;
+            builder.UserID = "sa";
+            builder.Password = "Luis";
+
+            var connectionString = builder.ToString();
+
+            using(SqlConnection sql= new SqlConnection(connectionString))
+            {
+                sql.Open();
+            }
+            Console.ReadLine();
         }
     }
   
